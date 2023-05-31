8b420b3c9a364cd47d3d3ee54881a5e4dddd2b95
Ethan Range <ethan@ethanrange.com>
2023-05-31 21:36:16 +0100

    Add test suite support

--- OpticML/test/Tests.hs
+++ OpticML/test/Tests.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+import Test.Tasty
+import Test.Tasty.HUnit
+import OpticML
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Testing OpticML" [lensUnitTests]
+
+lensUnitTests = testGroup "Lens Unit Tets (run via HUnit)"
+    [ testCase "view p1" $
+        view p1 (1, 2) @?= 1
+    , testCase "set p1" $
+        set p1 3 (1, 2) @?= (3, 2)
+    , testCase "over p1" $
+        over p1 (+1) (1, 2) @?= (2, 2)
+    ]
