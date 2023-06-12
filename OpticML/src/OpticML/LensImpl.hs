c53b7ac291959f5bf6e884df9bd100262432405e
Ethan Range <ethan@ethanrange.com>
2023-06-12 19:09:48 +0100

    Implement linear, addition and identity lenses

--- OpticML/src/OpticML/LensImpl.hs
+++ OpticML/src/OpticML/LensImpl.hs
@@ -1,10 +1,20 @@
 module OpticML.LensImpl
-  ( assocL, assocR,
+  ( identityL,
+    assocL, assocR,
     p1, p2,
+    add,
+    linear
   )
 where
 
 import OpticML.Lenses (Lens, lens)
+import Control.Monad (join)
+import Data.Matrix
+
+-- Identity Lens
+
+identityL :: Lens a a a a
+identityL = lens id snd
 
 -- Associative Lenses
 
@@ -33,3 +43,29 @@ p1 = lens fst $ \(n, (_, b)) -> (n, b)
 p2 :: Lens (x, a) (x, b) a b
 p2 = lens snd $ \(n, (a, _)) -> (a, n)
 {-# INLINE p2 #-}
+
+-- Addition Lens
+
+add :: Num a => Lens (a, a) (a, a) a a
+add = lens v u
+    where
+        v :: Num i => (i, i) -> i
+        v = uncurry (+)
+
+        u :: Num i => (i, (i, i)) -> (i, i)
+        u = join (,) . fst
+        
+linear :: Num a => Lens (Matrix a, Matrix a) (Matrix a, Matrix a) (Matrix a) (Matrix a)
+linear = lens v u
+    where
+        v :: Num a => (Matrix a, Matrix a) -> Matrix a
+        v = uncurry multStd
+
+        u :: Num a => (Matrix a, (Matrix a, Matrix a)) -> (Matrix a, Matrix a)
+        u (y, (m, x)) = (outer y x, multStd (transpose m) y)
+
+        outer :: Num a => Matrix a -> Matrix a -> Matrix a
+        outer v1 v2 = fromLists [[u * v | v <- ex v2] | u <- ex v1]
+            where
+                ex :: Matrix a -> [a]
+                ex = head . toLists . transpose
